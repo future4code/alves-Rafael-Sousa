5fae62313ea4ace5ef691faa834b7bf0a8959f38
Rafael santos de sousa <rafael.santos259@hotmail.com>
2022-05-22 17:15:16 -0300

    revisão postinho completa porem sem os desafios

--- modulo1/revisao-postinho/index.js
+++ modulo1/revisao-postinho/index.js
@@ -41,42 +41,44 @@ console.log(verificaSeEMaior(321, 2156));
 // CONDICIONAIS
 
 // Exercício 3------------------------------------------------------------------------------------
+const cadastro = () => { 
+    
     let nomeUser = prompt("digite o nome de usuario")
     let senha = (prompt("digite uma senha com 6 caracteres"))
-    let idade = Number (prompt("digite seu ano de Nascimento"))
+    let ano = Number (prompt("digite seu ano de Nascimento"))
     let paisOrigem =prompt("digite sua nacionalidade")
     
-    let arraycadastro = ["nomeUsuario", "senhaUsuario", "anoNascimento", "nacionalidade"]
+let idade = 2022 - ano
+        
+      
+            const usuario = []
+ 
+             usuario.push(nomeUser, senha, ano, paisOrigem)
+        
+             if(usuario[1].length > 6 && idade >= 18 && usuario[3] === "Brasileiro"){
+              
+                 console.log("conta cadastrada com sucesso")
+             }
 
+            else{
+                console.log("Conta não cadastrada")
+            }
+        }
 
- function cadastro (idade1,senha1){
-
-
-switch (arraycadastro){
-
-   case paisOrigem === 'Brasil':
-       console.log('Brasileiro')
-   break;
-   case idade1.idade >= 2004:
-       console.log('voce é maior de idade')
-       break;
-       case senha1.senha >= 6:
-       console.log(`sua senha é ${senha}`)
-   default:
-       console.log("cadastro não autorizado tente novamente")
-       break
-}
-
- }
-    
-console.log(cadastro)
+console.log(cadastro())
 
 // Exercício 4-----------------------------------------------------------------------------------------------
 
 const login = () => {
     const login = "labenu"
-    //  Sua lógica aqui
+let verifica = prompt("digite sua senha")
 
+if(login === verifica){
+    console.log("senha está correta")
+}
+else {
+    console.log("senha invalida")
+}
 }
 
 console.log(login());
@@ -85,8 +87,32 @@ console.log(login());
 
 const primeiraDose = () => {
 
-    //  Sua lógica aqui
+   let nome = prompt("digite seu nome")
+   let vacinaTomada= prompt("qual vacina você tomou?")
+   let coronavac = "coronavac"
+   let astrazenica = "astrazenica"
+   let pfizer= "pfizer"
 
+if(vacinaTomada === coronavac){
+    var tempo = 28
+   var  data = "18/06/2022"
+console.log(`Olá ${nome}! A próxima dose da ${vacinaTomada} é daqui a ${tempo} dias. Compareça no posto na data ${data}.`)
+}
+
+if(vacinaTomada === astrazenica){
+  var  tempo = 90
+  var  data = "22/08/2022"
+
+   console.log(`Olá ${nome}! A próxima dose da ${vacinaTomada} é daqui a ${tempo} dias. Compareça no posto na data ${data}.`)
+}
+
+if(vacinaTomada=== pfizer){
+    var  tempo = 90
+    var  data = '22/08/2022'
+  
+     console.log(`Olá ${nome}! A próxima dose da ${vacinaTomada} é daqui a ${tempo} dias. Compareça no posto na data ${data}.`)
+  
+}
 
 }
 console.log(primeiraDose())
@@ -104,23 +130,51 @@ const segundaDose = (nomeDoUsuario) => {
     ]
 
     //  Sua lógica aqui
+    
+        console.log(usuarios[1].imunizacao.replace("incompleta", "completa"))
+    
+    
+    }
 
-
-}
 console.log(segundaDose("Barbara"));
 
 // Exercício 7 --------------------------------------------------------------------------------------
 
-const avisoAosAtrasados = () => {
-    const usuarios = [
-        { nome: "Artur", imunizacao: "incompleta" },
-        { nome: "Barbara", imunizacao: "completa" },
-        { nome: "Carlos", imunizacao: "incompleta" }
-    ]
 
-    //  Sua lógica aqui
+    const avisoAosAtrasados = () => {
+        const usuarios = [
+            { nome: "Artur", imunizacao: "incompleta" },
+            { nome: "Barbara", imunizacao: "completa" },
+            { nome: "Carlos", imunizacao: "incompleta" }
+        ]
+    
+        let imunizacao = [
+            { completa: "completa" },
+            { incompleta: "incompleta"}
+        ]
+    
+        if (usuarios[0].imunizacao == "completa") {
+            console.log(`Olá ${usuarios[0].nome}, "sua imunização está" ${imunizacao[1].incompleta}!`)
+        } else {
+            console.log (`Olá ${usuarios[0].nome}! Sua imunização está ${imunizacao[1].incompleta} , por favor volte ao postinho para tomar a segunda dose.`)
+        }
+    
+        if (usuarios[1].imunizacao == "completa") {
+            console.log(`Olá ${usuarios[1].nome}, sua imunização está ${imunizacao[0].completa}!`)
+        } else {
+            console.log (`Olá ${usuarios[1].nome}! Sua imunização está ${imunizacao[1].incompleta}, por favor volte ao postinho para tomar a segunda dose.`)
+        }
+    
+        if (usuarios[2].imunizacao == "completa") {
+            console.log(`Olá ${usuarios[2].nome}, sua imunização está ${imunizacao[1].incompleta}!`)
+        } else {
+            console.log (`Olá ${usuarios[2].nome}! Sua imunização está ${imunizacao[1].incompleta} , por favor volte ao postinho para tomar a segunda dose.`)
+        }
+    
+    
+    }
+ 
 
-}
 console.log(avisoAosAtrasados());
 
 
