bfd8dc98b9ae8e69a85660ed1b1647f329f34855
Rafael santos de sousa <rafael.santos259@hotmail.com>
2022-08-09 19:05:27 -0300

    exercicio node-package-json

---
+++ modulo4/node-package-json/exercicio1/index.js
@@ -0,0 +1,8 @@
+const nome = ("rafael")
+const idade= Number("21")
+ 
+const calculo = idade + 7
+
+console.log("seu nome é", nome, "você tem", idade, "de idade, e daqui a 7 anos tera", calculo, "anos" )
+
+
