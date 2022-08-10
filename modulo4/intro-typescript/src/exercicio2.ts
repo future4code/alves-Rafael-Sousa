05e436e774d4a9db4959001bb65e9847b8e2d260
Rafael santos de sousa <rafael.santos259@hotmail.com>
2022-08-10 16:23:07 -0300

    intro typescript

---
+++ modulo4/intro-typescript/src/exercicio2.ts
@@ -0,0 +1,10 @@
+
+
+const imprimeTresCoresFavoritas =():string[] =>{
+    const cor1:string = "black"
+    const cor2:string = "red"
+    const cor3:string = "blue"
+    
+    return([cor1, cor2,cor3])
+ }
+ console.log(imprimeTresCoresFavoritas())
