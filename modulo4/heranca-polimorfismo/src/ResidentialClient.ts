5ba889c2877839dccc63126ad75c192cdde8f3d3
Rafael santos de sousa <rafael.santos259@hotmail.com>
2022-09-07 15:15:23 -0300

    exercicio heranca-polimorfismo terminado

---
+++ modulo4/heranca-polimorfismo/src/ResidentialClient.ts
@@ -0,0 +1,24 @@
+import { Client } from "./client";
+import { Residence } from "./residence";
+
+export class ResidentialClient extends Residence implements Client {
+  constructor(
+    private cpf: string,
+    residentsQuantity: number,
+    cep: string,
+    public name: string,
+    public registrationNumber: number,
+    public consumedEnergy: number,
+
+  ) {
+    super(residentsQuantity, cep);
+  }
+
+  public calculateBill(): number {
+    return this.consumedEnergy * 0.75;
+  }
+
+  public getCpf(): string {
+    return this.cpf;
+  }
+}
--- modulo4/heranca-polimorfismo/src/index.ts
+++ modulo4/heranca-polimorfismo/src/index.ts
@@ -28,4 +28,57 @@ class User {
       public getName(): string {
           return this.name
       }
+      public getPassowrd(): string {
+        return this.password
+    }
+    public interoduceYourself(): string {
+        return `Olá eu me chamo ${this.name}. Bom Dia!`
+     }
+ }
+    
+
+  const pessoa = new User ("rafael", "rafael@gmail.com", "rafael santos", "rafael123")
+
+//   console.log(pessoa.getEmail())
+//   console.log(pessoa.getId())
+//   console.log(pessoa.getName())
+  
+  //1) A) não seria possivel.
+  // B) vai ser impresso uma unica vez
+
+  class Customer extends User {
+    public purchaseTotal: number = 0;
+    private creditCard: string;
+  
+    constructor(
+      id: string,
+      email: string,
+      name: string,
+      password: string,
+      creditCard: string
+    ) {
+      super(id, email, name, password);
+      console.log("Chamando o construtor da classe Customer");
+      this.creditCard = creditCard;
+    }
+  
+    public getCreditCard(): string {
+      return this.creditCard;
+    }
   }
+
+  const usuario = new Customer ("pedro", "sdadasda@gmail.com", "Pedro", "123456", "511511151155")
+
+console.log(usuario.getCreditCard())
+console.log(usuario.getId())
+console.log(usuario.getName())
+console.log(usuario.getEmail())
+console.log(usuario.getPassowrd())
+console.log(usuario.interoduceYourself())
+//2) A) foi impresso uma unica vez no terminal
+
+//B) foi impresso duas vezes no terminal pois ela recebe os valores do pai e tbm os próprios valores
+
+//3) A) pelo oque eu entendi a senha poderia ser impressa mas mostraria a senha da subClass no caso a Customer
+
+
